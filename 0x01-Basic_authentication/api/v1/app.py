e0c0e372f712e0bf22539ccaac45580abec554e9
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-19 16:51:04 +0100

    task 1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,7 +23,9 @@ def not_found(error) -> str:
 @app.errorhandler(401)
 def unauthorized_error_handler(error) -> str:
     """to show unathorized user's page"""
-    return jsonify({"error": "Unauthorized"}), 401
+    return jsonify({
+        "error": "Unauthorized"
+        }), 401
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
