2aebc4adb7eef5a9d6544daa8074c6e51d59ef01
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-19 23:23:43 +0100

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """
+    Authorization class
+    Method:
+        require_auth(path, excluded_paths): returns False
+        authorization_header(request): returns None
+        current_user(request): returns None
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """to require authorization"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """add authorization credentials"""
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """assert current user"""
+        return None
+
