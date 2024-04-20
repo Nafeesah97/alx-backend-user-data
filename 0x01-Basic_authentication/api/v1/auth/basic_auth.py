7394bcf2cfcd907e3f59100f5b7224ffb99b237a
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-20 18:05:18 +0100

    decoder

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@
 """ Module of Basic Auth
 """
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """
@@ -20,3 +20,16 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string"""
+        if (
+                base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header).decode('utf-8')
+            return decoded
+        except:
+            return None
