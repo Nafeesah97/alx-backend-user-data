1569c553a2bae18b473ddb0694d9eb0eaf8c3f6b
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-21 00:41:39 +0100

    validate user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Module of Basic Auth
 """
-from typing import Set, Union
+from typing import Set, Union, TypeVar
 from api.v1.auth.auth import Auth
 import base64
-
+from models.user import User
 
 class BasicAuth(Auth):
     """
@@ -51,3 +51,24 @@ class BasicAuth(Auth):
             return (None, None)
         else:
             return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password"""
+        if (
+                user_email is None or
+                not isinstance(user_email, str)):
+            return None
+        if (
+                user_pwd is None or
+                not isinstance(user_pwd, str)):
+            return None
+        user = User.search({'email': user_email})
+        
+        if user is None:
+            return None
+        
+        if not user.is_valid_password(user_pwd):
+            return None
+        
+        return user        
