6db0cfc54b520c4d26579e307ccc8bd3c16e16a8
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-20 01:21:32 +0100

    extract basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,15 @@ class BasicAuth(Auth):
     """
     A basic authentication class
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization header"""
+        if authorization_header is None or not isinstance(
+            authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header.split(" ")[1]
+
