b4db673a9ce9353760fe58e71118ecf7693276f5
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-21 23:54:06 +0000

    fix

--- 0x02-Session_authentication/main_0.py
+++ 0x02-Session_authentication/main_0.py
@@ -1,19 +1,32 @@
-#!/usr/bin/env python3
-""" Main 0
+#!/usr/bin/python3
+""" Check response
 """
-import base64
-from api.v1.auth.basic_auth import BasicAuth
-from models.user import User
+import requests
 
-""" Create a user test """
-user_email = "bob@hbtn.io"
-user_clear_pwd = "H0lbertonSchool98!"
-
-user = User()
-user.email = user_email
-user.password = user_clear_pwd
-print("New user: {}".format(user.id))
-user.save()
-
-basic_clear = "{}:{}".format(user_email, user_clear_pwd)
-print("Basic Base64: {}".format(base64.b64encode(basic_clear.encode('utf-8')).decode("utf-8")))
+if __name__ == "__main__":
+    r = requests.get('http://0.0.0.0:3456/api/v1/status')
+    if r.status_code != 200:
+        print("Wrong status code: {}".format(r.status_code))
+        exit(1)
+    if r.headers.get('content-type') != "application/json":
+        print("Wrong content type: {}".format(r.headers.get('content-type')))
+        exit(1)
+    
+    try:
+        r_json = r.json()
+        
+        if len(r_json.keys()) != 1:
+            print("Not the right number of element in the JSON: {}".format(r_json))
+            exit(1)
+        
+        status_value = r_json.get('status')
+        if status_value is None:
+            print("Missing 'status' key in the JSON: {}".format(r_json))
+            exit(1)
+        if status_value != "OK":
+            print("'status' doesn't have the right value: {}".format(status_value))
+            exit(1)
+            
+        print("OK", end="")
+    except:
+        print("Error, not a JSON")
