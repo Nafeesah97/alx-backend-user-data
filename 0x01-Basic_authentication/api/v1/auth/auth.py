92781a00f6ebfe3754cce4040af1c2968dcc6219
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-21 02:40:37 +0100

    req *

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,13 +18,13 @@ class Auth:
         """to require authorization"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-        if path.endswith("/"):
-            path = path
-        else:
-            path = path + "/"
-        if path in excluded_paths:
-            return False
-        else:
+        
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("*") and path.startswith(excluded_path[:-1]):
+                return False
+            elif path == excluded_path or path == excluded_path[-1]:
+                return False
+
             return True
 
     def authorization_header(self, request=None) -> str:
