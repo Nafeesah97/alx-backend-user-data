bc047f393cb27161c2b2f6b9b4cf551527ca7d2f
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-21 03:04:17 +0100

    req *3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,8 +20,7 @@ class Auth:
             return True
         for excluded_path in excluded_paths:
             if (excluded_path.endswith("*") and
-                path.startswith(excluded_path[:-1])
-        ):
+                path.startswith(excluded_path[:-1])):
                 return False
             elif path == excluded_path or path == excluded_path[-1]:
                 return False
