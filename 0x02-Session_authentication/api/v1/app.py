9950b3c2e8687317343a746cc45b2e49e7637047
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-22 00:25:49 +0100

    session task 1 4

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,11 +13,6 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-known_routes = [
-    '/api/v1/status/',
-    '/api/v1/unauthorized/',
-    '/api/v1/forbidden/'
-    ]
 if getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
@@ -29,16 +24,22 @@ if getenv("AUTH_TYPE") == "auth":
 @app.before_request
 def before_request():
     """Executes before each request"""
-    if auth is None:
-        return
-    if request.path in known_routes:
-        return
-    if auth.require_auth(request.path, known_routes):
-        request.current_user = auth.current_user(request)
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if request.current_user is None:
-            abort(403)
+    if auth:
+        excluded_paths = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
+
 
 
 @app.errorhandler(404)
