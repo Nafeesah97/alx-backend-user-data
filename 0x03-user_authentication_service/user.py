ee6f1b2ba31459e67735725ede77b5607ca5b672
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-23 23:39:07 +0100

    user

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+user module
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+class User(Base):
+    """
+    This is a user class.
+    Method:
+
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
+
+    def __repr__(self):
+        """representation"""
+        return "{} - {}".format(self.id, self.email)
