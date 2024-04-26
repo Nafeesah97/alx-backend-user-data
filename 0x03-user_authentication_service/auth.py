f146a6b48be9c452264ded957109a28efd522c71
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-26 23:36:22 +0100

    f2

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,6 @@ module"""
 import bcrypt
 
 
-def _hash_password(password) -> bytes:
+def _hash_password(password: str) -> bytes:
     """hashes a password to a salt"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
