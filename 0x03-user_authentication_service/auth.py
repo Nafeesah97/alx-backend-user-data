8c4035d9804a38598885c191dc4958e031640e5c
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-27 01:53:14 +0100

    session f1

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,13 +56,10 @@ class Auth:
     def create_session(self, email: str) -> str:
         """Creates a new session for a user.
         """
-        user = None
         try:
             user = self._db.find_user_by(email=email)
+            session_id = str(uuid4())
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except NoResultFound:
             return None
-        if user is None:
-            return None
-        session_id = _generate_uuid()
-        self._db.update_user(user.id, session_id=session_id)
-        return session_id
